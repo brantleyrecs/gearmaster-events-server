9f32fc78c083f3696c8faa4e7c9a34390273fb20
Michael Brantley <brantleyrecs@gmail.com>
2024-02-15 19:07:02 -0600

    event model

---
+++ gearmasterapi/models/event.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .user import User
+from .type import Type
+
+class Event(models.Model):
+  name = models.CharField(max_length=55)
+  date = models.DateField(auto_now=True)
+  time = models.TimeField(auto_now=True)
+  location = models.CharField(max_length=200)
+  user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=0, related_name='user')
+  type = models.ForeignKey(Type, on_delete=models.SET_DEFAULT, default=0, related_name='type')
